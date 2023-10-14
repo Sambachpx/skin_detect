813477af6c6a2929c8761efc8dd480a72e2587ef
Sambachpx <samba.champroux@protonmail.com>
2023-10-14 15:26:34 +0200

    feat: add import lucide icons & implementation of screenOptions

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Home, Settings} from 'lucide-react-native';
 
 const bottomTab = createBottomTabNavigator();
 
@@ -34,7 +35,19 @@ function SettingsScreen() {
 const App = () => {
   return (
     <NavigationContainer>
-      <bottomTab.Navigator>
+      <bottomTab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: () => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'Home';
+              return <Home size={20} />;
+            } else if (route.name === 'Settings') {
+              iconName = 'Settings';
+              return <Settings size={20} />;
+            }
+          },
+        })}>
         <bottomTab.Screen name="Home" component={HomeScreen} />
         <bottomTab.Screen name="Settings" component={SettingsScreen} />
       </bottomTab.Navigator>
