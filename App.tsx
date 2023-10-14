0863e949a179f7707d1b07530f955eb8cd580681
Sambachpx <samba.champroux@protonmail.com>
2023-10-14 13:25:05 +0200

    feat: add home screen & settings screen with NavigationContainer & bottomTab.Screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const App = () => {
+const bottomTab = createBottomTabNavigator();
+
+function HomeScreen() {
   return (
     <View
       style={{
@@ -9,8 +13,32 @@ const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello, world!</Text>
+      <Text>Home</Text>
     </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <bottomTab.Navigator>
+        <bottomTab.Screen name="Home" component={HomeScreen} />
+        <bottomTab.Screen name="Settings" component={SettingsScreen} />
+      </bottomTab.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
